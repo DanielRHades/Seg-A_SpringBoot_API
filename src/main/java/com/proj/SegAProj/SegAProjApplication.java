40ce613da0b33b2eee2ec61e5dc5580e09b813ec
Daniel Rodriguez <drodriguez442@unab.edu.co>
2024-10-17 19:51:13 -0500

    Primer_Commit

---
+++ src/main/java/com/proj/SegAProj/SegAProjApplication.java
@@ -0,0 +1,13 @@
+package com.proj.SegAProj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegAProjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegAProjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/proj/SegAProj/SegAProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proj.SegAProj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegAProjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
